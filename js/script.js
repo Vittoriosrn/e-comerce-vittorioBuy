0ccd066b1178b125ad8bd85b7462c2cabc9f806f
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2023-07-20 07:49:32 -0300

    atualização no JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const b_aplicar_filtro = document.querySelector("#aplicar_filtro")
+const b_aplicar_filtro = document.querySelector("#aplicar_filtro")  // Botão para aplicar o filtro
 
 
 // Função para FILTRAR os produtos
@@ -6,7 +6,7 @@ function filtrar(marca){
     const filtro_marca = document.querySelectorAll(".filtro_marca")
     const produto = document.querySelectorAll(".produto")
 
-    console.log(marca)
+    //console.log(marca)
     for(let j = 0; j < produto.length; j++){
         if(!produto[j].outerText.includes(marca)){ //verifica se não contem o nome da marca para ocutar
             produto[j].classList.add("hide");
@@ -25,26 +25,27 @@ b_aplicar_filtro.addEventListener("click", () => {
             case "Asus":
                 if(filtro_marca[i].checked){
                     filtrar("Asus")  // Envia a marca selecionada para função
+                    console.log(filtro_marca[i])
                 }
-            break
             case "HyperX":
                 if(filtro_marca[i].checked){
                     filtrar("HyperX")
+                    console.log(filtro_marca[i])
                 }
-            break
             case "Redragon":
                 if(filtro_marca[i].checked){
                     filtrar("Redragon")
+                    console.log(filtro_marca[i])
                 }
-            break
             case "AMD":
                 if(filtro_marca[i].checked){
                     filtrar("AMD")
+                    console.log(filtro_marca[i])
                 }
-            break
             case "Husky":
                 if(filtro_marca[i].checked){
                     filtrar("Husky") 
+                    console.log(filtro_marca[i])
                 }
         }
     }
