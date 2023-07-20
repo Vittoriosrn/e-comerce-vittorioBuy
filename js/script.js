de01abacf1d714087b14bd95bc027f25ed701b1e
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2023-07-20 10:40:20 -0300

    atualização na função filtrar

--- js/script.js
+++ js/script.js
@@ -2,53 +2,42 @@ const b_aplicar_filtro = document.querySelector("#aplicar_filtro")  // Botão pa
 
 
 // Função para FILTRAR os produtos
-function filtrar(marca){
-    const filtro_marca = document.querySelectorAll(".filtro_marca")
-    const produto = document.querySelectorAll(".produto")
+function filtrar(marcas){
+    const filtro_marca = document.querySelectorAll(".filtro_marca") // todos os filtros
+    const produto = document.querySelectorAll(".produto") // todos os produtos em amostra
 
-    //console.log(marca)
-    for(let j = 0; j < produto.length; j++){
-        if(!produto[j].outerText.includes(marca)){ //verifica se não contem o nome da marca para ocutar
-            produto[j].classList.add("hide");
+    if(marcas.length > 0){
+        for(let p = 0; p < produto.length; p++){
+            for(let m = 0; m < marcas.length; m++){
+                if(!produto[p].outerText.includes(marcas[m])){
+                    produto[p].classList.add("hide")
+                }else{
+                    produto[p].classList.remove("hide")
+                }
+            }
+        }
+    }else{
+        for(let p = 0; p < produto.length; p++){  // For para remover o hide de cada produto
+            produto[p].classList.remove("hide")
         }
     }
 }
 
-// ==================== EVENTOS ================
+// ==================== EVENTOS ====================
 
 // Evento FILTRAR
 b_aplicar_filtro.addEventListener("click", () => {
     const filtro_marca = document.querySelectorAll(".filtro_marca")
+    const produtos_checked = [] // Array para armazena os filtros selecionado
+    let n_checked = 0 // Esse indice define a posição dos filtros checados no array
     
     for(let i = 0; i < filtro_marca.length; i++){ // For percorre todos os filtros para checar qual está selecionado
-        switch(filtro_marca[i].id){
-            case "Asus":
-                if(filtro_marca[i].checked){
-                    filtrar("Asus")  // Envia a marca selecionada para função
-                    console.log(filtro_marca[i])
-                }
-            case "HyperX":
-                if(filtro_marca[i].checked){
-                    filtrar("HyperX")
-                    console.log(filtro_marca[i])
-                }
-            case "Redragon":
-                if(filtro_marca[i].checked){
-                    filtrar("Redragon")
-                    console.log(filtro_marca[i])
-                }
-            case "AMD":
-                if(filtro_marca[i].checked){
-                    filtrar("AMD")
-                    console.log(filtro_marca[i])
-                }
-            case "Husky":
-                if(filtro_marca[i].checked){
-                    filtrar("Husky") 
-                    console.log(filtro_marca[i])
-                }
+        if(filtro_marca[i].checked){
+            produtos_checked[n_checked] = filtro_marca[i].id
+            n_checked++
         }
     }
+    filtrar(produtos_checked)
 })
 
 
